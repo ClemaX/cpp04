0fa903c70ba9fccd2c43f800226b18e3ef45b29e
Clément Hamada <clementhamada@protonmail.com>
2021-02-19 16:25:50 +0100

    ex03: MateriaSource: Case-insensitive Materia type

--- ex03/MateriaSource.cpp
+++ ex03/MateriaSource.cpp
@@ -32,16 +32,30 @@ void		MateriaSource::learnMateria(AMateria*const materia)
 		materies.push_back(materia->clone());
 }
 
+int			iscmp(std::string const& a, std::string const& b)
+{
+	std::string::const_iterator			it_a = a.begin();
+	std::string::const_iterator			it_b = b.begin();
+	const std::string::const_iterator	end_a = a.end();
+
+	while (it_a != end_a && std::tolower(*it_a) == std::tolower(*it_b))
+	{
+		it_a++;
+		it_b++;
+	}
+	return (std::tolower(*it_b) - std::tolower(*it_a));
+}
+
 AMateria*	MateriaSource::createMateria(std::string const& type)
 {
 	const std::vector<AMateria*>::const_iterator	end = materies.end();
 	std::vector<AMateria*>::const_iterator			it = materies.begin();
 	AMateria*										newMateria = NULL;
 
-	while (it != end && *it != NULL && (*it)->getType() != type)
+	while (it != end && *it != NULL && iscmp((*it)->getType(), type) != 0)
 		it++;
 
-	if (it != end)
+	if (it != end && *it != NULL)
 		newMateria = (*it)->clone();
 
 	return newMateria;
