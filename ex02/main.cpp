59698b5c7c69572c1e70ba13ab9ad8ff8180575e
Clément Hamada <clementhamada@protonmail.com>
2021-02-24 15:14:29 +0100

    ex02: Improve tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,16 +7,29 @@ int main()
 	ISpaceMarine *bob = new TacticalMarine;
 	ISpaceMarine *jim = new AssaultTerminator;
 
-	ISquad *vlc = new Squad;
+	Squad *vlc = new Squad;
+
+	std::cout << std::endl << *vlc << std::endl;
+
+	vlc->push(bob);
 	vlc->push(bob);
 	vlc->push(jim);
+
+	std::cout << std::endl << *vlc << std::endl;
+
 	for (int i = 0; i < vlc->getCount(); ++i)
 	{
 		ISpaceMarine *cur = vlc->getUnit(i);
+
+		std::cout << std::endl;
+
 		cur->battleCry();
 		cur->rangedAttack();
 		cur->meleeAttack();
 	}
+
+	std::cout << std::endl;
+
 	delete vlc;
 
 	//system("../leaks.sh tests");
